fcda23c955503947450a28a76b3a58630216062c
Nick Marko <nick.marko@prospection.com.au>
2023-01-27 21:20:58 +1100

    Add security and ability to exclude songs

--- src/main/java/com/staticvoid/songsuggestion/service/SongSuggestionService.java
+++ src/main/java/com/staticvoid/songsuggestion/service/SongSuggestionService.java
@@ -3,21 +3,22 @@ package com.staticvoid.songsuggestion.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.staticvoid.image.domain.Image;
 import com.staticvoid.image.recognition.service.ImageRecognitionService;
+import com.staticvoid.image.repository.ImageRepository;
 import com.staticvoid.songsuggestion.domain.Song;
 import com.staticvoid.songsuggestion.domain.SongDto;
 import com.staticvoid.songsuggestion.repository.SongRepository;
 import com.staticvoid.text.service.TextGenerationService;
 import com.youtube.service.YoutubeService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -28,26 +29,33 @@ public class SongSuggestionService {
     @Autowired
     private SongRepository songRepository;
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     public SongSuggestionService() {
         this.textGenerationService = new TextGenerationService();
         this.imageRecognitionService = new ImageRecognitionService();
         this.youtubeService = new YoutubeService();
     }
 
-    public final Song[] songSuggestions(List<String> tags, String imageId) {
+    public final Song[] songSuggestions(List<String> tags, String imageId, Boolean refresh) {
         //TODO get these prompts from resource files
         //TODO try removing bias by taking out song suggestion
         //TODO work on prompt design so it doesn't return any fake songs
         String prompt = String.format("List 5 real songs in json format that are associated with these tags: %s \n use this json format" + "\n [\n" + "{\n" + "\"name\": \"Empire State of Mind\",\n" + "\"artist\": \"Jay-Z ft. Alicia Keys\",\n" + "\"tags\": [\"city\", \"urban\", \"skyscraper\", \"metropolis\", \"downtown\"]\n" + "}]", tags.toString());
-
+        if(refresh) {
+            Song[] existingSongs = songRepository.findByImage_id(imageId).toArray(new Song[0]);
+            String additionalPrompt = String.format("\n Do not include any of these songs %s", toPromptString(existingSongs));
+            prompt = prompt + additionalPrompt;
+        }
         return getSongEntities(prompt, imageId);
     }
 
     public final Song[] songSuggestions(List<String> tags) {
-        return songSuggestions(tags, null);
+        return songSuggestions(tags, null, false);
     }
 
-    public final Song[] songSuggestions(List<String> tags, String imageId, List<String> genres) {
+    public final Song[] songSuggestionsWithGenres(List<String> tags, String imageId, List<String> genres) {
         String prompt = String.format("List 5 real songs in json format that are associated with these tags: %s" + "\n in these genres: %s" + " \n use this json format" + "\n [\n" + "{\n" + "\"name\": \"Empire State of Mind\",\n" + "\"artist\": \"Jay-Z ft. Alicia Keys\",\n" + "\"tags\": [\"city\", \"urban\", \"skyscraper\", \"metropolis\", \"downtown\"]\n" + "}]", tags.toString(), genres.toString());
 
         return getSongEntities(prompt, imageId);
@@ -86,12 +94,21 @@ public class SongSuggestionService {
 
     public final Song[] songSuggestions(Image image) {
         List<String> tags = imageRecognitionService.detectImageLabels(image);
-        return songSuggestions(tags, image.getId());
+        return songSuggestions(tags, image.getId(), false);
     }
 
-    public final Song[] songSuggestions(Image image, List<String> genres) {
+    public final Song[] songSuggestionsWithGenres(Image image, List<String> genres) {
         List<String> tags = imageRecognitionService.detectImageLabels(image);
-        return songSuggestions(tags, image.getId(), genres);
+        return songSuggestionsWithGenres(tags, image.getId(), genres);
+    }
+
+    public final Song[] reloadSongSuggestions(Image image) {
+        List<String>tags = imageRecognitionService.detectImageLabels(image);
+        return songSuggestions(tags, image.getId(), true);
+    }
+
+    private String toPromptString(Song[] song) {
+        return Arrays.stream(song).map(Song::toPromptString).collect(Collectors.toList()).toString();
     }
 
 
