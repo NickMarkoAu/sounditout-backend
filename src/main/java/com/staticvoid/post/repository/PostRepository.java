7c5efda78cdf2aa635cb5245ae5157cdac09188b
Nick Marko <nick.marko@prospection.com.au>
2023-05-21 12:19:49 +1000

    adjust post query to not include blocked users

--- src/main/java/com/staticvoid/post/repository/PostRepository.java
+++ src/main/java/com/staticvoid/post/repository/PostRepository.java
@@ -28,8 +28,27 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Long countAllByUser(ApplicationUser user);
 
     //TODO add privacy, will need to join to see if user is following
-    @Query(value = "SELECT * FROM post p INNER JOIN user u ON p.user_id=u.id WHERE LOWER(p.content) LIKE CONCAT('%',LOWER(?1),'%') OR LOWER(p.tags) LIKE CONCAT('%',LOWER(?1),'%') OR LOWER(u.name) LIKE CONCAT('%', LOWER(?1), '%') AND u.id NOT IN(?2) ORDER BY p.date DESC",
-            countQuery = "SELECT COUNT(*) FROM post p INNER JOIN user u ON p.user_id=u.id WHERE LOWER(p.content) LIKE CONCAT('%',LOWER(?1),'%') OR LOWER(p.tags) LIKE CONCAT('%',LOWER(?1),'%') OR LOWER(u.name) LIKE CONCAT('%', LOWER(?1), '%') AND u.id NOT IN(?2)",
+    @Query(value = "SELECT * " +
+            "FROM post p " +
+            "INNER JOIN applicationuser u ON p.user_id = u.id " +
+            "LEFT JOIN user_blocked_users bu ON u.id = bu.user_id AND bu.blockedusers_id = ?3 " +
+            "WHERE (" +
+            "    LOWER(p.content) LIKE CONCAT('%', LOWER(?1), '%') " +
+            "    OR LOWER(p.tags) LIKE CONCAT('%', LOWER(?1), '%') " +
+            "    OR LOWER(u.name) LIKE CONCAT('%', LOWER(?1), '%')" +
+            ") " +
+            "AND (u.id NOT IN (?2) AND bu.user_id IS NULL)" +
+            "ORDER BY p.date DESC",
+            countQuery = "SELECT COUNT(*) " +
+                    "FROM post p " +
+                    "INNER JOIN applicationuser u ON p.user_id = u.id " +
+                    "LEFT JOIN user_blocked_users bu ON u.id = bu.user_id AND bu.blockedusers_id = ?3 " +
+                    "WHERE (" +
+                    "    LOWER(p.content) LIKE CONCAT('%', LOWER(?1), '%') " +
+                    "    OR LOWER(p.tags) LIKE CONCAT('%', LOWER(?1), '%') " +
+                    "    OR LOWER(u.name) LIKE CONCAT('%', LOWER(?1), '%')" +
+                    ") " +
+                    "AND (u.id NOT IN (?2) AND bu.user_id IS NULL)",
             nativeQuery = true)
     Page<Post> search(String query, List<Long> blockedUsers, Pageable pageable);
 }
